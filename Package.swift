099c832d75d557f049a43bb87a8d1e7dab90c6c9
Dan Williams <dan@supapass.com>
2021-01-13 10:54:30 +0000

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,29 @@
+
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "OpenCastSwift",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "OpenCastSwift iOS",
+            targets: ["OpenCastSwift iOS"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", .upToNextMajor(from: "5.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "OpenCastSwift iOS",
+            dependencies: [
+                "swift-protobuf",
+                "SwiftyJSON"
+            ],
+            path: "Source"
+        ),
+    ]
+)
